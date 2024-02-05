752f4a048dd05acb746b30dc07f61070494a01e2
Nilesh Agarwal <nick.aggarwal@gmail.com>
2024-02-05 23:59:55 +0530

    Create input_schema.py

---
+++ input_schema.py
@@ -0,0 +1,14 @@
+{
+  "inputs": [
+    {
+      "name": "file_name",
+      "datatype": "BYTES",
+      "shape": [
+        1
+      ],
+      "data": [
+        "https://inferless-public.s3.amazonaws.com/ss_gt_1.wav"
+      ]
+    }
+  ]
+}
