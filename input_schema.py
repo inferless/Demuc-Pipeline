d513212dec21680b0e7fd4a598e57f0f30bc9065
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-09-17 02:51:44 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -1,14 +1,13 @@
-{
-  "inputs": [
-    {
-      "name": "file_name",
-      "datatype": "BYTES",
-      "shape": [
-        1
-      ],
-      "data": [
-        "https://inferless-public.s3.amazonaws.com/ss_gt_1.wav"
-      ]
-    }
-  ]
+INPUT_SCHEMA = {
+    "inputs":
+      {
+        "name": "file_name",
+        "datatype": "BYTES",
+        "shape": [
+          1
+        ],
+        "data": [
+          "https://inferless-public.s3.amazonaws.com/ss_gt_1.wav"
+        ]
+      }
 }
